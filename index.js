90c58542b68a573635d188b9c3058b98082aab3f
ThuyLe12101995 <ThuyLe95>
2018-07-29 16:39:21 +0700

    add file router

---
+++ index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa');
+const cors = require('@koa/cors');
+const koaBody = require|('koa-body');
+const router = require('./Router');
+const serve   = require('koa-static');
+
+let app = new Koa();
+
+app.use(koaBody({
+    multipart : true,
+}));
+
+app.use(cors());
+
+app.use(serve(__dirname + '/public' ));
+app.use(router.routes);
+app.listen(process.env.PORT || 3001);
